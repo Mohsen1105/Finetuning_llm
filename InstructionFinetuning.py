5e3f29003f54901481d51defb8bf0158e552943c
Mohsen1105 <9998082+Mohsen1105@users.noreply.github.com>
2024-06-19 14:20:22 +0000

    complete import

---
+++ InstructionFinetuning.py
@@ -0,0 +1,9 @@
+import itertools
+import jsonlines
+
+from datasets import load_dataset
+from pprint import pprint
+
+from llama import BasicModelRunner
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
